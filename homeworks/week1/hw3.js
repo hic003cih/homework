ca2f39b58cc3a38dceeb5893ecac6a784f834da4
leif <leif.lf.chang@foxsemicon.com>
2018-11-23 15:31:47 +0800

    finish wk1 hw3

--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,3 +1,17 @@
 export const isPrime = (n) => {
-  return false;
+  if(n===0 | n===1| n===null | n <0){
+    return false;
+  }else{
+    if(n===2){
+      return true;
+    }else{
+      for(i=2 ;i<n;i++){
+        if(n % i === 0){
+          return false;
+        }
+      }
+        return true;
+    }
+  }
+  
 }
