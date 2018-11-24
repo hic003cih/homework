0931666d202d2b8ca07386ecaeb548769c8f403f
leif <leif.lf.chang@foxsemicon.com>
2018-11-24 18:00:27 +0800

    edit wk1 hw5 ver1

--- homeworks/week1/hw5.js
+++ homeworks/week1/hw5.js
@@ -1,3 +1,20 @@
 export const add = (a, b) => {
+
+  let temp1 = a.split('').reverse();
+  let temp2 = b.split('').reverse();
+  let sum=[];
+  if(temp1.length>temp2.length){
+    for(let i = 0; i < temp1.length; i++){
+
+      sum[i] = (Number(temp1[i]) + Number(temp2[i]));
+      console.log('sum[i]' + sum[i]);
+      if(sum[i]>=10){
+        sum[i+1] = 1;
+        console.log('sum' + sum[i+1]);
+      }
+    }
+  }
+
+
   return parseInt(a, 10) + parseInt(b, 10);
 }
